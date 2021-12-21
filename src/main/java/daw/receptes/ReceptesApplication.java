6f3b788c0fdcf6656a05dc3ea6a78133c521e269
Guillermo Marcó <guille.marco.m@gmail.com>
2021-12-21 21:32:52 +0100

    Modificació de ReceptesApplication.class pel desplegament correcte a Tomcat

--- src/main/java/daw/receptes/ReceptesApplication.java
+++ src/main/java/daw/receptes/ReceptesApplication.java
@@ -2,12 +2,19 @@ package daw.receptes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ReceptesApplication {
+public class ReceptesApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReceptesApplication.class, args);
 	}
+        
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+            return builder.sources(ReceptesApplication.class);
+        }
 
 }
